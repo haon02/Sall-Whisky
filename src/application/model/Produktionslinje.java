50fb188aeae7fa8c15e881bae73e22abfbba1e96
Nicolai <nicolaiknudsen@outllok.dk>
2026-05-03 10:51:05 +0200

    updated Produktionlinje

--- src/application/model/Produktionslinje.java
+++ src/application/model/Produktionslinje.java
@@ -1,4 +1,13 @@
 package application.model;
 
+import java.util.Map;
+
 public class Produktionslinje {
+    private Map<Korn, Double> kornMap;
+    private Map<Korn, Double> gærMap;
+    private double vandMængdeMiliLiter;
+    private int mæskeTidMinutter;
+    private Map<Medarbejder, Double> medarbejderMap;
+    private int idProduktionslinje;
+
 }
