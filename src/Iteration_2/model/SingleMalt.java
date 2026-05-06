39465fa76d0766060c4025e6a5516662a29344f4
haon02 <noahvad@gmail.com>
2026-05-06 09:39:13 +0200

    yessir hashmapzzzz

--- src/Iteration_2/model/SingleMalt.java
+++ src/Iteration_2/model/SingleMalt.java
@@ -1,4 +1,20 @@
 package Iteration_2.model;
 
-public class SingleMalt {
+import java.util.HashMap;
+
+public class SingleMalt implements DestillatType {
+    HashMap<Destillat,Double> destillater;
+
+    public SingleMalt(HashMap<Destillat, Double> destillater) {
+        this.destillater = destillater;
+    }
+
+    @Override
+    public HashMap<Destillat, Double> getIndhold() {
+        return null;
+    }
+
+    public HashMap<Destillat, Double> getDestillater() {
+        return destillater;
+    }
 }
